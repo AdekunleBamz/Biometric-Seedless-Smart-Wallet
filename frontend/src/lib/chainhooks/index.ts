00de374a2d5a628e51a0499d92b1e4960fe9a5cb
walterthesmart <nwaugowalter@gmail.com>
2025-12-20 06:19:38 +0100

    feat: add ObserverConfig type

--- frontend/src/lib/chainhooks/index.ts
+++ frontend/src/lib/chainhooks/index.ts
@@ -1,5 +1,9 @@
 import { ChainhooksClient } from '@hirosystems/chainhooks-client';
 
+export type ObserverConfig = {
+    name: string;
+};
+
 export const createObserver = () => {
     // Logic placeholder
 };
